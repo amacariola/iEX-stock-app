64a37b6985cd913dae2aa7ed308227dc5945f4f1
Ace Macariola <falconsys32@gmail.com>
2021-05-02 09:43:56 -0400

    Done with db

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
 
-  config.action_mailer.perform_deliveries = false
+  #config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
   config.action_mailer.delivery_method = :smtp
