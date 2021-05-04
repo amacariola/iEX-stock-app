2d4cd24ec1298d9e7906dc66940e8e79b16aad55
Ace Macariola <falconsys32@gmail.com>
2021-05-04 13:01:42 -0400

    New tests added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,8 +38,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name:     'falconsys32@gmail.com',
-    password:      'Kuromacariola',
+    user_name:     ENV['GMAIL_USER'],
+    password:      ENV['GMAIL_PASS'],
     domain:        'gmail.com',
     address:       'smtp.gmail.com',
     port:          '587',
